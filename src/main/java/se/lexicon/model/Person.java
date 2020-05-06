7b04d878a497d0d2f36efd8794e1709fb17bffbe
The-Popa <tobias.carlstrom@bredband2.com>
2020-05-06 10:56:58 +0200

    Second Try

---
+++ src/main/java/se/lexicon/model/Person.java
@@ -0,0 +1,33 @@
+package se.lexicon.model;
+
+public class Person {
+    private final int personID;
+    private String firstName;
+    private String lastName;
+
+    public Person(int personID, String firstName, String lastName) {
+        this.personID = personID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getPersonID() {
+        return personID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
---
+++ src/test/java/se/lexicon/model/PersonTest.java
@@ -0,0 +1,31 @@
+package se.lexicon.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PersonTest {
+    public final int personID = 741016;
+    public String firstName = "Tobias";
+    public String lastName = "Carlström";
+
+    @Test
+    public void getPersonID() {
+    }
+
+    @Test
+    public void getFirstName() {
+    }
+
+    @Test
+    public void setFirstName() {
+    }
+
+    @Test
+    public void getLastName() {
+    }
+
+    @Test
+    public void setLastName() {
+    }
+}
